7626ad7a9bbecf616a299ebf9976569285c8ee54
B. Endres <endres@systopia.de>
2019-08-28 18:19:29 +0200

    added DB data structure

---
+++ CRM/Shadowdata/Upgrader.php
@@ -0,0 +1,30 @@
+<?php
+/*-------------------------------------------------------+
+| SYSTOPIA SHADOW DATA EXTENSION                         |
+| Copyright (C) 2019 SYSTOPIA                            |
+| Author: B. Endres (endres@systopia.de)                 |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+use CRM_Shadowdata_ExtensionUtil as E;
+
+/**
+ * Collection of upgrade steps.
+ */
+class CRM_Shadowdata_Upgrader extends CRM_Shadowdata_Upgrader_Base {
+
+  /**
+   * Create databases
+   */
+  public function install() {
+    $this->executeSqlFile('sql/shadowdata_contact.sql');
+  }
+
+}
